ef0998c379eca464098bde2b21857b29c86e04f3
wangpeijian <519955464@qq.com>
2018-06-28 14:46:09 +0800

    初始化

--- src/main/java/com/org/serviceexport/api/Hello.java
+++ src/main/java/com/org/serviceexport/api/Hello.java
@@ -3,19 +3,25 @@ package com.org.serviceexport.api;
 import com.org.serviceexport.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Create by wangpeijian on 2018/6/22.
  */
 
+@RefreshScope
 @RequestMapping("hello")
 @RestController
 public class Hello {
 
     @Autowired
     private HelloService helloService;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Value("${foo}")
     String foo;
@@ -28,5 +34,10 @@ public class Hello {
     @RequestMapping("config")
     public String config(){
         return foo;
+    }
+
+    @RequestMapping("rest")
+    public String rest(){
+        return restTemplate.getForObject("http://service-user/user/hello?name={1}", String.class, "zhenkeng");
     }
 }
